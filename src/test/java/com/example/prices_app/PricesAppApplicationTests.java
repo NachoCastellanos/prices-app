249d2aa5c7dcc59d87155f372892005b57ec6d72
Nacho Castellanos Moreno <nacho2casmor@gmail.com>
2025-01-14 10:58:59 +0100

    chore(prices-app): initial commit with the Spring project

---
+++ src/test/java/com/example/prices_app/PricesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prices_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
